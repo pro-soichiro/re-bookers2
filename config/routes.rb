05b3f68ce3aa840397dfb22938343de720ce0708
pro-soichiro <dr.1987626@gmail.com>
2021-09-18 02:06:15 +0000

    [Update] アプリケーションに必要なファイルの準備とファイルの設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :books, only: [:index,:show,:edit]
+  resources :users, only: [:index,:show,:edit]
+  resources :homes, only: [:top,:about]
 end
